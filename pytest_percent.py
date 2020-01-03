ec8b89550b58e0fa0d31832f2266a42d4370c536
dillonm197 <dillon.miller@swiftpage.com>
2020-01-03 14:45:04 -0700

    Add tests. Fix formula. Add pytest fixture.

--- pytest_percent.py
+++ pytest_percent.py
@@ -5,28 +5,40 @@ import logging
 import pytest
 
 LOGGER = logging.getLogger(__name__)
+REQUIRED_PERCENT = 100
 
 
 def pytest_addoption(parser):
     parser.addoption(
         '--required-percent',
         action='store',
-        type=int,
-        choices=range(101),
         help='Percent of tests required to pass.',
     )
 
 
-@pytest.mark.trylast
-def pytest_sessionfinish(session, exitstatus):
-    required_percent = session.config.getoption('--required-percent')
-    if isinstance(required_percent, int):
-        passed_percent = 100 - ((session.testsfailed * 100) / session.testscollected)
-        if passed_percent >= required_percent:
-            LOGGER.info(f'{passed_percent}% of tests passed, required {required_percent}%.')
-            exitstatus = pytest.ExitCode.OK
-        else:
-            LOGGER.error(f'{passed_percent}% of tests passed, required {required_percent}%.')
+def pytest_configure(config):
+    global REQUIRED_PERCENT
+    REQUIRED_PERCENT = float(config.getoption('--required-percent'))
+    if REQUIRED_PERCENT <= 1.0:
+        REQUIRED_PERCENT *= 100
+    REQUIRED_PERCENT = int(REQUIRED_PERCENT)
+    LOGGER.info(f'Required percent set to {REQUIRED_PERCENT}%')
 
 
-__version__ = '0.0.1'
+@pytest.hookimpl(trylast=True)
+def pytest_sessionfinish(session):
+    testspassed = session.testscollected - session.testsfailed
+    passed_percent = int((testspassed / session.testscollected) * 100)
+    if passed_percent >= REQUIRED_PERCENT:
+        LOGGER.info(f'{passed_percent}% of tests passed, required {REQUIRED_PERCENT}%.')
+        session.exitstatus = pytest.ExitCode.OK
+    else:
+        LOGGER.error(f'{passed_percent}% of tests passed, required {REQUIRED_PERCENT}%.')
+
+
+@pytest.fixture(scope='session')
+def required_percent():
+    return REQUIRED_PERCENT
+
+
+__version__ = '0.0.2'
---
+++ tests/test_percent.py
@@ -0,0 +1,18 @@
+def test_one(required_percent):
+    assert isinstance(required_percent, int)
+
+
+def test_two():
+    assert True
+
+
+def test_three():
+    assert True
+
+
+def test_four():
+    assert True
+
+
+def test_five():
+    assert False
